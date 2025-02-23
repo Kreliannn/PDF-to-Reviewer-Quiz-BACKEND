4b5466bbd4ecaf5b210a29350e0b318e8d0cf19f
Kreliannn <krelianquimson@gmail.com>
2025-02-23 14:58:30 +0800

    connect to frontned

--- index.ts
+++ index.ts
@@ -4,12 +4,11 @@ import dotenv  from "dotenv";
 import multer from "multer";
 import fs from "fs"
 import pdf from "pdf-parse"
-
-const upload = multer({ dest: 'uploads/' })
-
-const app = express()
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 dotenv.config()
+
+const app = express()
 
 const PORT = process.env.PORT || 4000;
 
@@ -18,14 +17,44 @@ app.use(cors({
     credentials : true
 }))
 
+const upload = multer({ dest: 'uploads/' })
+
+const genAI = new GoogleGenerativeAI(process.env.GEMENI_API_KEY as string);
+
+const model = genAI.getGenerativeModel({ 
+    model: "gemini-1.5-flash",
+    systemInstruction : "create 5 item and defenition for my reviewer based on the given lesson. return it in json format [{item : string, defenition : string}, {item : string, defenition : string}]" 
+});
+
 
 app.get("/",  (request, response)=> {
-    response.send("hello world")
+    response.send("hello")
 })
 
-app.post("/upload", upload.single('file'), (request, response) => {
-    console.log("success")
-    response.send("sucess")
+app.post("/upload", upload.single('file'), async (request, response) => {
+
+    if(!request.file)
+    {
+        response.send("error")
+        return
+    } 
+
+    const { file } = request
+    const buffer = fs.readFileSync("./uploads/" + file.filename)
+
+    const pdfToString = (await pdf(buffer)).text
+
+    const data =  await model.generateContent(pdfToString)
+
+    const aiResponse = data.response.text()
+
+    const formatResponse = aiResponse.match(/\[.*\]/s);
+
+    const finalResponse = (formatResponse) ? JSON.parse(formatResponse[0]) : "";
+
+    fs.unlinkSync("./uploads/" + file.filename)
+
+    response.send(finalResponse)
 })
 
 
